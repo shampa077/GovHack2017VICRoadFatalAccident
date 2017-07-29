6387f7905ab62abb0d8db623144f6e95421e456f
Shampa Shahriyar <shampa.shahriyar@monash.edu>
2017-07-29 23:24:06 +1000

    ok

---
+++ sript.js
@@ -0,0 +1,6 @@
+
+var accidentRef = document.getElementById("accident");
+accidentRef.onclick = function() {
+    window.location.href = 'https://public.tableau.com/profile/shampa#!/vizhome/Accident_4/VictimStatistics';
+};
+
