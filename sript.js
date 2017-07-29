956f5696c898d3b8eafd5883697bb432650605d6
Shampa Shahriyar <shampa.shahriyar@monash.edu>
2017-07-29 23:39:59 +1000

    ok

--- sript.js
+++ sript.js
@@ -2,6 +2,7 @@
 function doit()
 {
     window.location.href = "https://public.tableau.com/profile/shampa#!/vizhome/Accident_4/VictimStatistics";
+    console.log("accident pressed");
     
 }
 //var accidentRef = document.getElementById("accident");
